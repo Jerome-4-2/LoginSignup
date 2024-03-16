508447d8cbc8f18d4af65774866f1e243a4f0790
Jerome-4-2 <103257489+Jerome-4-2@users.noreply.github.com>
2024-03-16 14:15:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LoginSignup from './components/Loginsignup/LoginSignup';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup/>
+      
+    </div>
+  );
+}
+
+export default App;
